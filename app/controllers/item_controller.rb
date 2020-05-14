cd77c387adfe518a151c35b9ad762d89f5eb9d2a
mitsuhiro0602 <0hornet0@gmail.com>
2020-05-15 02:42:28 +0900

    item_controller_repaire

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -7,6 +7,32 @@ class ItemController < ApplicationController
     def step1
       @item = Item.new
       @item.images.new
+      @item.build_brand
+      @category_parent_array = Category.where(ancestry: nil)
+    end
+
+    #親カテゴリーが選択された後に動くアクション
+    def get_category_children
+      #選択された親カテゴリーに紐づく子カテゴリーの配列を取得する
+      @category_children = Category.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
+    end
+    # 子カテゴリーが選択された後に動くアクション
+    def get_category_grandchildren
+      #選択された子カテゴリーに紐づく孫カテゴリーの配列を取得する
+      @category_children = Category.find("#{params[:child_id]}").children
+    end
+
+    #孫カテゴリーが選択された後に動くアクション
+    def get_size
+      selected_grandchild = Category.find("#{params[:grandchild_id]}") #孫カテゴリーを取得する
+      if related_size_parent = selected_grandchild.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得する
+        @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得する
+      else
+        selected_child = Category.find("#{params[:grandchild_id]}").parent #孫カテゴリーの親を取得する
+        if related_size_parent = selected_child.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得する
+          @sizes = related_size_parent.children  #紐づいたサイズ（親）の子供の配列を取得する
+        end
+      end
     end
 
     def create
@@ -14,7 +40,7 @@ class ItemController < ApplicationController
       if @item.save
         redirect_to root_path, notice: "出品しました"
       else
-        render :new
+        redirect_to step1_item_index, alert: "必須項目を入力してください"
       end
     end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
-    has_many :images
+    has_many :images, allow_destroy: true
+    belongs_to :user
+    belongs_to :category, optional: true
+    belongs_to :brand, optional: true
+    belongs_to :size, optional: true
+    belongs_to :measure, optional: true
     accepts_nested_attributes_for :images, allow_destroy: true
 end
