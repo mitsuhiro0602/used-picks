218fb215b9c69dd4a28fe4363babcd941b94ed77
mitsuhiro0602 <0hornet0@gmail.com>
2020-06-29 18:34:22 +0900

    sidebar_create

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sidebar_link_item(name, path)
+    # binding.pry
+    class_name = 'channel'
+    class_name << 'active' if current_page?(path)
+
+    content_tag :li, class:class_name do
+      link_to name, path, class: 'channel_name'
+    end
+  end
 end
