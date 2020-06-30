9ec8b853927488a98c25932b9c0fc0ffc35259ed
mitsuhiro0602 <0hornet0@gmail.com>
2020-06-30 19:56:48 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
